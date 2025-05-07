954430e9dd908758a4af89923770dcbd8fa28c64
panghuTT <742926413@qq.com>
2025-05-07 14:12:19 +0800

    SpringBoot脚手架

---
+++ src/Admin/z-log-admin/src/main/java/com/zlog/admin/ZLogAdminApplication.java
@@ -0,0 +1,13 @@
+package com.zlog.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZLogAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZLogAdminApplication.class, args);
+    }
+
+}
---
+++ src/Admin/z-log-admin/src/test/java/com/zlog/admin/ZLogAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlog.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZLogAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
