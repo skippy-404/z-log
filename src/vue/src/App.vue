20556cefd0b60e62c928031a0a4d22254b577795
panghuTT <742926413@qq.com>
2025-05-07 21:56:33 +0800

    Router初步v1
    四个页面，请在views下细化，只给出了初始示例，App.vue需要删掉图标还未改

--- src/vue/src/App.vue
+++ src/vue/src/App.vue
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <router-view />
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
