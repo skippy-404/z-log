881aa2c04ed526d2f2af4dfc4b7a672906951908
skippy <skippy1010177@gmail.com>
2025-07-03 11:36:42 +0800

    fix 乱码

--- src/Admin/z-log-admin/src/main/java/com/zlog/admin/ZLogAdminApplication.java
+++ src/Admin/z-log-admin/src/main/java/com/zlog/admin/ZLogAdminApplication.java
@@ -1,9 +1,15 @@
 package com.zlog.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@MapperScan("com.zlog.admin.mapper")
+@EntityScan("com.zlog.admin.entity")
+@EnableJpaRepositories("com.zlog.admin.repository")
 public class ZLogAdminApplication {
 
     public static void main(String[] args) {
